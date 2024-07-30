cddaaf4075c150b68d933302185b38577bf018f7
Jyoti.Patil <jyoti.patil@avon.com>
2024-07-30 16:33:35 +0530

    added workflow

--- infrastructure/all/backend.tf
+++ infrastructure/all/backend.tf
@@ -1,10 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "Qualys_backend"
-    key    = "terraform.tfstate"
+    bucket = "qualysbackend"
     region = "us-east-1"
-    encrypt = true
-    dynamodb_table = "Qualys_backend"
     }
   }
 
